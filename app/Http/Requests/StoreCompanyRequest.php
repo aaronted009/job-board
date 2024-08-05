57e7869bf50d7271b23b279f8850915b9d810c4e
Aaron <aaronpromail@gmail.com>
2024-08-05 19:23:14 +0100

    define validation rules for Company

--- app/Http/Requests/StoreCompanyRequest.php
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -22,7 +22,10 @@ class StoreCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
         ];
     }
 }
