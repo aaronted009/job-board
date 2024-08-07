3c6d666a52300ed77a47c1bf5a3235abef661718
Aaron <aaronpromail@gmail.com>
2024-08-07 22:29:24 +0100

    add validation rules for creating a User

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -22,7 +22,9 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string',
         ];
     }
 }
