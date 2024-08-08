d165485d9fb830dcd2aa18c936cba47c2b7af312
Aaron <aaronpromail@gmail.com>
2024-08-09 00:15:42 +0100

    add validation rules for creating an offer

--- app/Http/Requests/StoreOfferRequest.php
+++ app/Http/Requests/StoreOfferRequest.php
@@ -22,7 +22,10 @@ class StoreOfferRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'company_id' => 'required|integer|exists:companies,id',
+            'salary' => 'required|integer',
         ];
     }
 }
