d6004f41093e42340b68c8b6ec4789f5e9ec4a50
Aaron <aaronpromail@gmail.com>
2024-08-11 00:54:22 +0100

    delete an Employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -70,6 +70,7 @@ class EmployeeController extends Controller
      */
     public function destroy(Employee $employee)
     {
-        //
+        $employee->delete();
+        return response()->json(['message' => 'Employee deleted successfully']);
     }
 }
