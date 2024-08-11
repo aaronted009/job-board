015c19f31ee3d6e22a6c83dd0793da79814711cc
Aaron <aaronpromail@gmail.com>
2024-08-11 01:15:12 +0100

    update Employee model with mass assignable fields

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'phone',
+        'address',
+        'city',
+        'state',
+        'country',
+        'job_title',
+        'department',
+        'salary',
+    ];
 }
