6eb578bfad4fb1cb6081b4eaebbc5a75a40682f4
Aaron <aaronpromail@gmail.com>
2024-08-13 13:42:09 +0100

    add a page title to homepage

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -3,9 +3,11 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class Home extends Component
 {
+    #[Title('Accueil')] 
     public function render()
     {
         return view('livewire.home');
