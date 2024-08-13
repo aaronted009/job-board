9764d0087206ef0d899289216ed1c404dc173704
Aaron <aaronpromail@gmail.com>
2024-08-13 13:38:06 +0100

    Add start button to homepage

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,5 @@
-<div>
+<div
+    style="display: flex; justify-content: center; align-items: center; height: 100vh; background-image: url('path/to/your/image.jpg');">
     {{-- Do your work, then step back. --}}
+    <button>Commencer</button>
 </div>
