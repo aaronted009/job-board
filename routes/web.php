be7c38990c72b11e39a41ea07220219c254fe82a
Aaron <aaronpromail@gmail.com>
2024-08-14 17:12:10 +0100

    add route for Register

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Home;
+use App\Livewire\Register\Register;
 
 Route::get('/', Home::class)->name('home');
+
+Route::get('/register', Register::class)->name('register');
