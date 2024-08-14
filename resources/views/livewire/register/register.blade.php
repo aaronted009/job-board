b0a748b4431008b19e2f97a31803db04f6ebd84f
Aaron <aaronpromail@gmail.com>
2024-08-14 19:04:02 +0100

    register page interface

--- resources/views/livewire/register/register.blade.php
+++ resources/views/livewire/register/register.blade.php
@@ -1,3 +1,13 @@
-<div>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
     {{-- Stop trying to control. --}}
+    <div class="card pt-3">
+        <div class="card-body">
+            <div class="d-flex flex-column align-items-center">
+                <a href="#" class="btn btn-primary mb-3">Créer un compte Entreprise</a>
+                <a href="#" class="btn btn-secondary mb-3">Créer un compte Travailleur</a>
+                <span>Vous avez déjà un compte ?<a href="#" class="btn btn-link">Connectez-vous</a></span>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
