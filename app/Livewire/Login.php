3cbf4398511747fa4a18bb46d066d74fe4605906
Aaron <aaronpromail@gmail.com>
2024-08-14 23:53:11 +0100

    Login logic implementation

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -3,11 +3,46 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class Login extends Component
 {
+    #[Validate('required')]
+    public $email = '';
+    #[Validate('required')]
+    public $password = '';
+
     public function render()
     {
         return view('livewire.login');
+    }
+
+    #[Title('Se connecter')]
+    public function login()
+    {
+        $this->validate();
+
+        
+        $user = DB::table('users')
+            ->where('email', $this->email)
+            ->first();
+        
+        $company = DB::table('companies')
+            ->where('email', $this->email)
+            ->where('password', $this->password)
+            ->first();
+
+        if ($user and Hash::check($this->password, $user->password)) {
+            session()->flash('success', 'Vous êtes connecté.');
+            return redirect()->route('home');
+        } else if ($company) {
+            session()->flash('success', 'Vous êtes connecté.');
+            return redirect()->route('home');
+        } else {
+            session()->flash('error', 'Email ou mot de passe incorrect.');
+            return redirect()->route('login');
+        }
     }
 }
