046f262cfbd2ba864731903310e7caf790c14ff4
Aaron <aaronpromail@gmail.com>
2024-08-14 19:29:53 +0100

    update routes for company & user registration on register page

--- resources/views/livewire/register/register.blade.php
+++ resources/views/livewire/register/register.blade.php
@@ -3,8 +3,8 @@
     <div class="card pt-3">
         <div class="card-body">
             <div class="d-flex flex-column align-items-center">
-                <a href="#" class="btn btn-primary mb-3">Créer un compte Entreprise</a>
-                <a href="#" class="btn btn-secondary mb-3">Créer un compte Travailleur</a>
+                <a href="{{ route('register.company') }}" class="btn btn-primary mb-3">Créer un compte Entreprise</a>
+                <a href="{{ route('register.user') }}" class="btn btn-secondary mb-3">Créer un compte Travailleur</a>
                 <span>Vous avez déjà un compte ?<a href="#" class="btn btn-link">Connectez-vous</a></span>
             </div>
         </div>
