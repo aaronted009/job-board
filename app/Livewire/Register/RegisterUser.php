c5c29d0393fd345193ee8d8c77a3bddbe9fbe227
Aaron <aaronpromail@gmail.com>
2024-08-14 19:19:48 +0100

    create RegisterUser component

---
+++ app/Livewire/Register/RegisterUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Register;
+
+use Livewire\Component;
+
+class RegisterUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.register.register-user');
+    }
+}
