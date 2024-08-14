8814c1b095db7d4ee7987e79a4506a712e507f8f
Aaron <aaronpromail@gmail.com>
2024-08-14 17:35:13 +0100

    add title to register page

--- app/Livewire/Register/Register.php
+++ app/Livewire/Register/Register.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Register;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class Register extends Component
 {
+    #[Title('S\'enregistrer')] 
     public function render()
     {
         return view('livewire.register.register');
