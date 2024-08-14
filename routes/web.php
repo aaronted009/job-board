b5adcc2ba75ba15525a44f6b1bd28fcb3382e6cd
Aaron <aaronpromail@gmail.com>
2024-08-14 19:23:21 +0100

    add routes for company & user registration

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Home;
 use App\Livewire\Register\Register;
+use App\Livewire\Register\RegisterCompany;
+use App\Livewire\Register\RegisterUser;
 
 Route::get('/', Home::class)->name('home');
 
 Route::get('/register', Register::class)->name('register');
+
+Route::get('/register/company', RegisterCompany::class)->name('register.company');
+
+Route::get('/register/user', RegisterUser::class)->name('register.user');
