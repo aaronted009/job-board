280eec0fd29b75ffb5a10b51689a8e8762cf5b23
Aaron <aaronpromail@gmail.com>
2024-08-18 22:26:19 +0100

    update dashboard interface for a user

--- resources/views/livewire/dashboard/dashboard-user.blade.php
+++ resources/views/livewire/dashboard/dashboard-user.blade.php
@@ -1,3 +1,10 @@
-<div>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
     {{-- Stop trying to control. --}}
+    <div class="card pt-3">
+        <div class="card-body">
+            <div class="d-flex flex-column align-items-center">
+                <a href="#" class="btn btn-success mb-3">Voir les offres</a>
+            </div>
+        </div>
+    </div>
 </div>
