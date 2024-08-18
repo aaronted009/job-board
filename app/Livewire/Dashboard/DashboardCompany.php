25ae18c93011c8d7d8acdfcb9dcc0e8404651c59
Aaron <aaronpromail@gmail.com>
2024-08-18 20:38:39 +0100

    create component for company dashboard

---
+++ app/Livewire/Dashboard/DashboardCompany.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use Livewire\Component;
+
+class DashboardCompany extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.dashboard-company');
+    }
+}
