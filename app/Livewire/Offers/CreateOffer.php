26e32f4ddf3767c39d596ace7c52dab8cb251835
Aaron <aaronpromail@gmail.com>
2024-08-18 22:47:43 +0100

    create Offer component

---
+++ app/Livewire/Offers/CreateOffer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Offers;
+
+use Livewire\Component;
+
+class CreateOffer extends Component
+{
+    public function render()
+    {
+        return view('livewire.offers.create-offer');
+    }
+}
