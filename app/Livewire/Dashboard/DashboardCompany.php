e8c38e73d63b795bc961bdd78e12fd422b0b6679
Aaron <aaronpromail@gmail.com>
2024-08-18 21:50:55 +0100

    update DashboardCompany component with data passed in

--- app/Livewire/Dashboard/DashboardCompany.php
+++ app/Livewire/Dashboard/DashboardCompany.php
@@ -3,9 +3,17 @@
 namespace App\Livewire\Dashboard;
 
 use Livewire\Component;
+use App\Models\Company;
 
 class DashboardCompany extends Component
 {
+    public Company $company;
+
+    public function mount($id)
+    {
+        $this->company = Company::findOrFail($id);
+    }
+
     public function render()
     {
         return view('livewire.dashboard.dashboard-company');
