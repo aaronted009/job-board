fe1cb83b534ecbfd81a4a958a1f8913c08c999be
Aaron <aaronpromail@gmail.com>
2024-08-21 00:45:29 +0100

    show company their offers

--- app/Livewire/Offers/ShowCompanyOffers.php
+++ app/Livewire/Offers/ShowCompanyOffers.php
@@ -3,11 +3,23 @@
 namespace App\Livewire\Offers;
 
 use Livewire\Component;
+use App\Models\Company;
+use App\Models\Offer;
 
 class ShowCompanyOffers extends Component
 {
+    public Company $company;
+
+    public function mount($id)
+    {
+        $this->company = Company::findOrFail($id);
+    }
+
     public function render()
     {
-        return view('livewire.offers.show-company-offers');
+        $companyOffers = Offer::where('company_id', $this->company->id)->get();
+        return view('livewire.offers.show-company-offers', [
+            'companyOffers' => $companyOffers,
+        ]);
     }
 }
--- resources/views/livewire/offers/show-company-offers.blade.php
+++ resources/views/livewire/offers/show-company-offers.blade.php
@@ -1,3 +1,16 @@
-<div>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
     {{-- Be like water. --}}
+    @foreach ($companyOffers as $offer)
+        <div class="card pt-3 mx-2">
+            <div class="card-body">
+                <div class="d-flex flex-column align-items-center">
+                    <div class="offer">
+                        <h2>{{ $offer->title }}</h2>
+                        <p>{{ $offer->description }}</p>
+                        <p>Salary: {{ $offer->salary }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 </div>
