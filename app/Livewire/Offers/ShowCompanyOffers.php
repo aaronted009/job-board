82f5433d723d3a04b3ed39ad4ec47a86c9bc58c4
Aaron <aaronpromail@gmail.com>
2024-08-20 23:55:58 +0100

    create ShowCompanyOffer component

---
+++ app/Livewire/Offers/ShowCompanyOffers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Offers;
+
+use Livewire\Component;
+
+class ShowCompanyOffers extends Component
+{
+    public function render()
+    {
+        return view('livewire.offers.show-company-offers');
+    }
+}
