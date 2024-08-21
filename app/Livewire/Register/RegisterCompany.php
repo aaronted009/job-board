4794bf70d759bc8042cb0e22aa4105c921d03fd8
Aaron <aaronpromail@gmail.com>
2024-08-21 18:40:30 +0100

    add title to RegisterCompany rendered page

--- app/Livewire/Register/RegisterCompany.php
+++ app/Livewire/Register/RegisterCompany.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Register;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class RegisterCompany extends Component
 {
+    #[Title('Enregistrer une société')] 
     public function render()
     {
         return view('livewire.register.register-company');
