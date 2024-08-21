c054ffb4e0ab5a9951c16d1143d2eb78c01139a1
Aaron <aaronpromail@gmail.com>
2024-08-21 18:42:16 +0100

    add title to RegisterUser rendered page

--- app/Livewire/Register/RegisterUser.php
+++ app/Livewire/Register/RegisterUser.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Register;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class RegisterUser extends Component
 {
+    #[Title('Enregistrer un utilisateur')] 
     public function render()
     {
         return view('livewire.register.register-user');
