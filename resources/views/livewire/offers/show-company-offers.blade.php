403a6c96a215166130a39f3c979bc74062020a28
Aaron <aaronpromail@gmail.com>
2024-08-21 19:57:16 +0100

    add conditional display of company offers in their dashboard
    if no offer is created yet, display so with possibility of creating one; else display the offers

--- resources/views/livewire/offers/show-company-offers.blade.php
+++ resources/views/livewire/offers/show-company-offers.blade.php
@@ -1,16 +1,30 @@
 <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
     {{-- Be like water. --}}
-    @foreach ($companyOffers as $offer)
+    @if ($companyOffers->isEmpty())
         <div class="card pt-3 mx-2">
             <div class="card-body">
                 <div class="d-flex flex-column align-items-center">
                     <div class="offer">
-                        <h2>{{ $offer->title }}</h2>
-                        <p>{{ $offer->description }}</p>
-                        <p>Salary: {{ $offer->salary }}</p>
+                        <h2>Vous n'avez pas encore créé d'offre</h2>
+                        <a href="{{ route('offers.create', ['id' => $company]) }}" class="btn btn-primary mb-3">Créer une
+                            offre</a>
                     </div>
                 </div>
             </div>
         </div>
-    @endforeach
+    @else
+        @foreach ($companyOffers as $offer)
+            <div class="card pt-3 mx-2">
+                <div class="card-body">
+                    <div class="d-flex flex-column align-items-center">
+                        <div class="offer">
+                            <h2>{{ $offer->title }}</h2>
+                            <p>{{ $offer->description }}</p>
+                            <p>Salary: {{ $offer->salary }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @endif
 </div>
