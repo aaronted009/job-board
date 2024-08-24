e052392faef66834817ce68a3ca80990f23ac1d6
Aaron <aaronpromail@gmail.com>
2024-08-24 23:30:53 +0100

    add link to offers list

--- resources/views/livewire/dashboard/dashboard-user.blade.php
+++ resources/views/livewire/dashboard/dashboard-user.blade.php
@@ -3,7 +3,7 @@
     <div class="card pt-3">
         <div class="card-body">
             <div class="d-flex flex-column align-items-center">
-                <a href="#" class="btn btn-success mb-3">Voir les offres</a>
+                <a href="{{ route('offers.list') }}" class="btn btn-success mb-3">Voir les offres</a>
             </div>
         </div>
     </div>
