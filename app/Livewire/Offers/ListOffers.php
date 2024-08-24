964ad2da1210e6784b9ba29a292ab41c4493ce83
Aaron <aaronpromail@gmail.com>
2024-08-24 23:09:10 +0100

    create ListOffers component

---
+++ app/Livewire/Offers/ListOffers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Offers;
+
+use Livewire\Component;
+
+class ListOffers extends Component
+{
+    public function render()
+    {
+        return view('livewire.offers.list-offers');
+    }
+}
