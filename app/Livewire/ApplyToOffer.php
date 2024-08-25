696ec1752926a03633a19320b792398f09d7f0ea
Aaron <aaronpromail@gmail.com>
2024-08-25 22:20:03 +0100

    create ApplyToOffer component

---
+++ app/Livewire/ApplyToOffer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ApplyToOffer extends Component
+{
+    public function render()
+    {
+        return view('livewire.apply-to-offer');
+    }
+}
