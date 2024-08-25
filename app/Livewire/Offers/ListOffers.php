970bb1aca3dcb1a44c3997655d0899983fcec540
Aaron <aaronpromail@gmail.com>
2024-08-25 20:57:51 +0100

    list all offers

--- app/Livewire/Offers/ListOffers.php
+++ app/Livewire/Offers/ListOffers.php
@@ -3,11 +3,15 @@
 namespace App\Livewire\Offers;
 
 use Livewire\Component;
+use App\Models\Offer;
 
 class ListOffers extends Component
 {
     public function render()
     {
-        return view('livewire.offers.list-offers');
+        $offers = Offer::all();
+        return view('livewire.offers.list-offers', [
+            'offers' => $offers,
+        ]);
     }
 }
