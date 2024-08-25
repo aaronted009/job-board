6192503ae1db7d820af7ffe8f85ba88180074c60
Aaron <aaronpromail@gmail.com>
2024-08-25 20:59:37 +0100

    add title to offers list page

--- app/Livewire/Offers/ListOffers.php
+++ app/Livewire/Offers/ListOffers.php
@@ -4,9 +4,11 @@ namespace App\Livewire\Offers;
 
 use Livewire\Component;
 use App\Models\Offer;
+use Livewire\Attributes\Title;
 
 class ListOffers extends Component
 {
+    #[Title('Liste des offres')]
     public function render()
     {
         $offers = Offer::all();
