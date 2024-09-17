8d527704129388fc2aeaeb9982636afddc77a0e5
Aaron <aaronpromail@gmail.com>
2024-09-17 05:06:37 +0100

    update application to offers logic - wip
    offers a user already applied to shouldn't be available again for application

--- app/Livewire/Offers/ListOffers.php
+++ app/Livewire/Offers/ListOffers.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Offers;
 
 use Livewire\Component;
 use App\Models\Offer;
+use App\Models\Application;
 use Livewire\Attributes\Title;
 
 class ListOffers extends Component
@@ -12,8 +13,10 @@ class ListOffers extends Component
     public function render()
     {
         $offers = Offer::all();
+        $applications = Application::all();
         return view('livewire.offers.list-offers', [
             'offers' => $offers,
+            'applications' => $applications //We need to pass the applications to the view to know whether the user has already applied to an offer
         ]);
     }
 }
